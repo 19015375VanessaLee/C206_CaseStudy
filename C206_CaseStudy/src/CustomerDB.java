e70ae560bee6d4014adeb68fe1704c498e478a9e
19012855 <19012855@DESKTOP-7LAN5OG>
2020-08-19 20:26:05 +0800

    Chris

--- C206_CaseStudy/src/CustomerDB.java
+++ C206_CaseStudy/src/CustomerDB.java
@@ -1,42 +1,12 @@
-import java.util.ArrayList;
 
-public class CustomerDB extends Customer{
-	ArrayList<Customer> customerListArrayList = new ArrayList<Customer>();
-	
-	public void addCustomer(Customer customer) {
-		if (customerListArrayList.contains(customer)==true) {
-			System.out.println("Customer existed!");
-		}else {
-			System.out.println("Customer added!");
-		}
-	}
-	public void viewAllCustomer() {
-		System.out.println(customerListArrayList);
-	}
-	public void delCustomer(String name) {
-		if(customerListArrayList.contains(name)==true) {
-			customerListArrayList.remove(name);
-			System.out.println("Customer deleted!");
-		}else {
-			System.out.println("Customer does not exist!");
-		}
-	}
-	public void ShowCustomerMenu() {
-		System.out.println("Add New Customer");
-		System.out.println("View All Customer");
-		System.out.println("Delete Customer");
-	}
-	public void processOption() {
-		
-	}
-	public Customer inputCustomerDetails() {
-		return null;
-		
-	}
-	public int selectCustomer() {
-		return 0;
-		
-	}
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 19012855, Aug 19, 2020 8:25:33 PM
+ */
+
+public class CustomerDB {
 
 }
-
